30e84425f60d7569732cb36dc8a993ce9f7898c5
BirkBerger <43404623+BirkBerger@users.noreply.github.com>
2026-04-10 15:50:45 +0200

    Parse player kills, assists and blindness.

--- lib/parseLog.ts
+++ lib/parseLog.ts
@@ -1,29 +1,35 @@
 import fs from 'fs';
 import path from 'path';
-import { ParsedLog, Player, Round, TeamNames } from '@/types/log.types';
+import { ParsedLog, Player, Players, Round, TeamNames } from '@/types/log.types';
 
 const enum MATCH_PROPERTY {
     ctTeam = 'ctTeam',
     terroristTeam = 'terroristTeam',
     roundStart = 'roundStart',
     roundEnd = 'roundEnd',
-    roundScore = 'roundScore'
+    roundScore = 'roundScore',
+    assist = 'assist',
+    kill = 'kill',
+    blindness = 'blindness',
 }
 
 type Matches = { [property in MATCH_PROPERTY]: RegExpMatchArray | null };
 
-const regexes: { [property in MATCH_PROPERTY]: RegExp } = {
-    roundStart: /(\d{2}:\d{2}:\d{2}): World triggered "Round_Start"/,
-    roundEnd: /(\d{2}:\d{2}:\d{2}): World triggered "Round_End"/,
-    roundScore: /MatchStatus: Score: (\d+):(\d+) on map "[^"]+" RoundsPlayed: (\d+)/,
-    ctTeam: /MatchStatus: Team playing "CT": (.+)/,
-    terroristTeam: /MatchStatus: Team playing "TERRORIST": (.+)/
+const regexes: { [ property in MATCH_PROPERTY ]: RegExp } = {
+    roundStart:     /(\d{2}:\d{2}:\d{2}): World triggered "Round_Start"/,
+    roundEnd:       /(\d{2}:\d{2}:\d{2}): World triggered "Round_End"/,
+    roundScore:     /MatchStatus: Score: (\d+):(\d+) on map "[^"]+" RoundsPlayed: (\d+)/,
+    ctTeam:         /MatchStatus: Team playing "CT": (.+)/,
+    terroristTeam:  /MatchStatus: Team playing "TERRORIST": (.+)/,
+    kill:           /"(?<name>[^<]+)<\d+><(?<id>[^>]+)><(?<side>[^>]*)>" \[[^\]]+\] killed "(?<victimName>[^<]+)<\d+><(?<victimId>[^>]+)><[^>]*>" \[[^\]]+\] with "(?<weapon>[^"]+)"/,
+    assist:         /"(?<name>[^<]+)<\d+><(?<id>[^>]+)><(?<side>[^>]*)>" (?:flash-)?assisted killing "(?<victimName>[^<]+)<\d+><(?<victimId>[^>]+)><[^>]*>"/,
+    blindness:      /"(?<name>[^<]+)<\d+><(?<id>[^>]+)><[^>]*>" blinded for (?<duration>[\d.]+)/,
 }
 
-export function parseLog(raw: string): ParsedLog {
+function parseLog(raw: string): ParsedLog {
     const lines = raw.split('\n');
 
-    let teamNames = null;
+    let teamNames: TeamNames = ["", ""];
     const rounds = new Array<Round>();
     const players: { [id: string]: Player } = {};
 
@@ -32,58 +38,104 @@ export function parseLog(raw: string): ParsedLog {
         terroristTeam: null,
         roundStart: null,
         roundEnd: null,
-        roundScore: null
+        roundScore: null,
+        kill: null,
+        assist: null,
+        blindness: null
     }
 
     for (const line of lines) {
 
-        matches.ctTeam = line.match(regexes.ctTeam) || matches.ctTeam;
-        matches.terroristTeam = line.match(regexes.terroristTeam) || matches.terroristTeam;
-        matches.roundStart = line.match(regexes.roundStart) ||  matches.roundStart;
-        matches.roundScore = line.match(regexes.roundScore) || matches.roundScore;
-
-        teamNames = getTeamNames(matches, teamNames);
-
+        matchLines(line, matches);
+        
         if (matches.roundScore) {
             const roundsPlayed = parseInt(matches.roundScore[3]);
-            if(roundsPlayed < 1) continue;
+            
+            // Extract the team names from the first round, recording the intial team sides
+            if (roundsPlayed == 0) teamNames = getTeamNames(matches, teamNames);
+            if(roundsPlayed >= 0) {
+                updatePlayers(players, teamNames, matches);
+                
+                matches.roundEnd = line.match(regexes.roundEnd);
+                updateRounds(rounds, matches, teamNames);
+            }
         }
 
-        matches.roundEnd = line.match(regexes.roundEnd);
 
-        const round = createRound(matches, teamNames);
-        if (round) rounds.push(round);
     }
 
     return {
-        teamNames: teamNames || ["", ""],
+        teamNames: teamNames,
         rounds,
         players
     }
-
 }
 
-function getTeamNames(matches: Matches, teamNames: TeamNames | null): TeamNames | null {
-    // Extract the first occurrence of team names which desides the intial team sides
+function matchLines(line: string, matches: Matches): Matches {
+    if (line.includes("MatchStatus")) {
+        matches.ctTeam = line.match(regexes.ctTeam) || matches.ctTeam;
+        matches.terroristTeam = line.match(regexes.terroristTeam) || matches.terroristTeam;
+        matches.roundScore = line.match(regexes.roundScore) || matches.roundScore;
+    } else if (line.includes("World triggered")) {
+        matches.roundStart = line.match(regexes.roundStart) ||  matches.roundStart;
+    } else if (line.includes("killed")) {
+        matches.kill = line.match(regexes.kill);
+    } else if (line.includes("assisted")) {
+        matches.assist = line.match(regexes.assist);
+    } else if (line.includes("blinded")) {
+        matches.blindness = line.match(regexes.blindness);
+    }
+
+    return matches;
+}
+
+
+function getTeamNames(matches: Matches, teamNames: TeamNames): TeamNames {
     if (!matches.ctTeam || !matches.terroristTeam) return teamNames;
-    return teamNames || [matches.ctTeam[1], matches.terroristTeam[1]] as TeamNames
+    return [matches.ctTeam[1], matches.terroristTeam[1]];
 }
 
-function createRound(matches: Matches, teamNames: TeamNames | null): Round | null {
-    if (!matches.roundStart || !matches.roundEnd || !matches.roundScore || !teamNames) return null;
+function updatePlayers(players: Players, teamNames: TeamNames, matches: Matches) {
+    if (matches.kill) {
+        const matchResult = createPlayer(players, teamNames, matches, matches.kill);
+        if (matchResult) players[matchResult.id].kills += 1;
+        matches.kill = null;
+    } else if (matches.assist) {
+        const matchResult = createPlayer(players, teamNames, matches, matches.assist);
+        if (matchResult) players[matchResult.id].assists += 1;
+        matches.assist = null;
+    } else if (matches.blindness) {
+        const matchResult = createPlayer(players, teamNames, matches, matches.blindness);
+        if (matchResult) players[matchResult.id].blindness += parseFloat(matchResult.duration);
+        matches.blindness = null;
+    }
+}
+
+function createPlayer(players: Players, teamNames: TeamNames, matches: Matches, match: RegExpMatchArray): { [key: string]: string } | null {
+    const matchResult = match?.groups;
+    if (!matchResult) return null;
+    if (!players[matchResult.id]) {
+        const teamName = (matches.ctTeam && matches.ctTeam[1] == matchResult.side) ? teamNames[0] : teamNames[1];
+        players[matchResult.id] = new Player(matchResult.name, teamName);
+    }
+    return matchResult;
+}
+
+function updateRounds(rounds: Round[], matches: Matches, teamNames: TeamNames) {
+    if (!matches.roundEnd || !matches.roundStart || !matches.roundScore || !teamNames) return;
 
     const startTime = matches.roundStart[1].split(":");
     const endTime = matches.roundEnd[1].split(":")
     const score1 = parseInt(matches.roundScore[1]);
     const score2 = parseInt(matches.roundScore[2]);
-    const hasTeamSidesChanged = matches.ctTeam && teamNames && matches.ctTeam[1] == teamNames[1];
-    return {
+    const hasTeamSidesChanged = matches.ctTeam && matches.ctTeam[1] == teamNames[1];
+    rounds.push ({
         duration: timeDiff(startTime, endTime),
         status: {
             teamSides: hasTeamSidesChanged ? [1, 0] : [0, 1],
             score: hasTeamSidesChanged ? [score2, score1] : [score1, score2]
         }
-    }
+    })
 }
 
 
--- types/log.types.ts
+++ types/log.types.ts
@@ -14,22 +14,29 @@ export interface HitGroupDamage {
     [ hitGroup: string ]: number
 };
 
-export interface Player {
-    name: string,
-    teamIndex: number,
-    kills: number,
-    deaths: number,
-    assists: number,
-    blindness: number,
-    WeaponUse: WeaponUse,
-    moneySpend: number,
-    hitGroupDamage: HitGroupDamage
+export class Player {
+    name: string = "";
+    teamName: string = "";
+    kills: number = 0;
+    deaths: number = 0;
+    assists: number = 0;
+    blindness: number = 0;
+    WeaponUse: WeaponUse = {};
+    moneySpend: number = 0;
+    hitGroupDamage: HitGroupDamage = {};
+
+    constructor(name: string, teamName: string) {
+        this.name = name;
+        this.teamName = teamName;
+    }
 };
+
+export type Players = { [id: string ]: Player };
 
 export type TeamNames = [string, string];
 
 export interface ParsedLog {
     teamNames: TeamNames
     rounds: Round[],
-    players: { [id: string ]: Player }
+    players: Players
 };
